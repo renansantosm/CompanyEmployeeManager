175110e7c8acc9162eced4cffbe6ad0580991fdb
renan <renan@DESKTOP-TPED91N>
2024-12-17 10:38:26 -0300

    Update Swagger authorize button description

--- CompanyEmployeeManager/Program.cs
+++ CompanyEmployeeManager/Program.cs
@@ -61,7 +61,8 @@ builder.Services.AddSwaggerGen(c =>
         Scheme = "Bearer",
         BearerFormat = "JWT",
         In = ParameterLocation.Header,
-        Description = "Bearer JWT",
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] " +
+                "and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
     });
     c.AddSecurityRequirement(new OpenApiSecurityRequirement()
     {
